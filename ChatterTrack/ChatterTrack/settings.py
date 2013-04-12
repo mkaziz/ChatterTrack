7441d88239d1479c5a69d9bdc21dc679f908db60
Amazon Instance <ubuntu@ip-10-250-20-188.us-west-2.compute.internal>
2013-04-12 00:04:31 +0000

    Added new projet to settings file

--- ChatterTrack/ChatterTrack/settings.py
+++ ChatterTrack/ChatterTrack/settings.py
@@ -124,7 +124,8 @@ INSTALLED_APPS = (
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
-    'south'
+    'south',
+    'ct'
 )
 
 # A sample logging configuration. The only tangible logging
