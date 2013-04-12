f5942ed3cec2bdaa49fc67aaf0777fc25f4039d5
Khalid <kaziz@u.northwestern.edu>
2013-04-11 23:17:46 -0500

    Now serving up test page with dev server

--- ChatterTrack/ct/urls.py
+++ ChatterTrack/ct/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from ct.views import index
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -14,5 +15,5 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),    
-    url(r'^ct/', include('ct.urls')),
+    url(r'^$', index),
 )
--- ChatterTrack/ct/views.py
+++ ChatterTrack/ct/views.py
@@ -1,1 +1,14 @@
 # Create your views here.
+from django.shortcuts import render, render_to_response
+from django.http import HttpResponse, HttpResponseBadRequest
+import json
+
+
+def createError(msg):
+  response_data = { "error" : -1, "message" : msg }
+  return HttpResponseBadRequest(content=json.dumps(response_data), content_type="application/json")
+  
+  
+def index(request):
+  response_data = { "error" : -1, "message" : "test" }
+  return HttpResponse(content=json.dumps(response_data), content_type="application/json")
