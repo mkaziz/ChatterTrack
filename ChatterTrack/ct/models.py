f37c2996dd20dc62170bd3c92e05929ea7285002
Khalid <kaziz@u.northwestern.edu>
2013-04-21 01:16:38 -0500

    Started adding twitter authentication code

--- ChatterTrack/ct/models.py
+++ ChatterTrack/ct/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+  user_id = models.CharField(max_length=60)
+  user_id.primary_key = True;
+  oauth_token = models.CharField(max_length=256)
+  oauth_secret = models.CharField(max_length=256)
+  screen_name = models.CharField(max_length=60)
--- ChatterTrack/ct/views.py
+++ ChatterTrack/ct/views.py
@@ -1,7 +1,7 @@
 # Create your views here.
 from django.shortcuts import render, render_to_response
 from django.http import HttpResponse, HttpResponseBadRequest
-import json
+import json, requests, twitter
 
 
 def createError(msg):
@@ -13,3 +13,19 @@ def index(request):
     response_data = { "error" : -1, "message" : "test" }
     return render(request, "index.html")
   #return HttpResponse(content=json.dumps(response_data), content_type="application/json")
+
+def twitter_login(request):
+    
+    consumerKey = "b4G5PKMYYFsFLIrrTsbA"
+    consumerSecret = "pSM2rv4TJDnC1pJKIZeU7W2mR3E3mOOuluZhnyn4"
+    
+    baseUrl = "https://api.twitter.com/"
+    method = "oauth/request_token"
+    params = {"oauth_callback" : twitter.oauth.urlencode_noplus("twitter")}
+    
+    #auth=OAuth(oauth_token, oauth_token_secret, CONSUMER_KEY, CONSUMER_SECRET)
+    auth = OAuth("", "", consumerKey, consumerSecret)
+    
+    auth.encode_params(baseUrl, method, params)
+    
+    return createError("it works!")
