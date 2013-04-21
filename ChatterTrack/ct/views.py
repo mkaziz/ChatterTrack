5935ae7f138d81a55ab8b43b272b587ca8eb40f3
Khalid <kaziz@u.northwestern.edu>
2013-04-21 15:53:44 -0500

    Temp commit, oauth still not working

--- ChatterTrack/ct/views.py
+++ ChatterTrack/ct/views.py
@@ -21,19 +21,35 @@ def twitter_login(request):
     
     baseUrl = "https://api.twitter.com/"
     method = "oauth/request_token"
-    params = { "oauth_callback" : quote("http://ec2-54-244-189-248.us-west-2.compute.amazonaws.com/ct/twitter_login_handler/",safe='') }
+    params = { "oauth_callback" : "http://ec2-54-244-189-248.us-west-2.compute.amazonaws.com/ct/twitter_login_handler/" }
     
     #auth=OAuth(oauth_token, oauth_token_secret, CONSUMER_KEY, CONSUMER_SECRET)
     auth = twitter.OAuth("", "", consumerKey, consumerSecret)
-
-    ep = "OAuth " + auth.encode_params(baseUrl, method, params)
+    oauth_token, oauth_token_secret = parse_oauth_tokens(
+        twitter.oauth.request_token())
     
-    response = requests.post(baseUrl+method, headers={ "Authorization" : ep})
+    encodedParams = dict(urlparse.parse_qsl(auth.encode_params(baseUrl, method, params)))
+    
+    headers={ "Authorization" : \
+    "OAuth oauth_callback=\"" + encodeWord(encodedParams["oauth_callback"]) + "\"," + \
+    "oauth_consumer_key=\"" + encodeWord(encodedParams["oauth_consumer_key"]) + "\"," + \
+    "oauth_nonce=\"" + encodeWord(encodedParams["oauth_nonce"]) + "\"," + \
+    "oauth_signature=\"" + encodeWord(encodedParams["oauth_signature"]) + "\"," + \
+    "oauth_signature_method=\"" + encodeWord(encodedParams["oauth_signature_method"]) + "\"," + \
+    "oauth_timestamp=\"" + encodeWord(encodedParams["oauth_timestamp"]) + "\"," + \
+    "oauth_version=\"" + encodeWord(encodedParams["oauth_version"]) + "\"" }
+    
+    response = requests.post(baseUrl+method, headers=headers.items())
     r = response.request.headers
     t = response.text
     
     return createError()
 
+def encodeWord(w):
+    if type(w) is unicode: w = w.encode('utf-8')
+    w = quote(w,safe='').replace("+", "%20")
+    return w
+    
 def encodeHeaders(query):
     
     headers = {}
@@ -50,3 +66,13 @@ def twitter_login_handler(request):
     
     
     return createError("reached twitter login handler")
+
+
+def parse_oauth_tokens(result):
+    for r in result.split('&'):
+        k, v = r.split('=')
+        if k == 'oauth_token':
+            oauth_token = v
+        elif k == 'oauth_token_secret':
+            oauth_token_secret = v
+    return oauth_token, oauth_token_secret
