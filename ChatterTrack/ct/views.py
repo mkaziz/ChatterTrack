4de4a5030a1e4b87dad0df9ce7944185f2069545
Amazon Instance <ubuntu@ip-10-250-20-188.us-west-2.compute.internal>
2013-04-22 21:30:51 +0000

    Fixed bugs so that iframes work properly on frontend code

--- ChatterTrack/ct/views.py
+++ ChatterTrack/ct/views.py
@@ -1,8 +1,8 @@
 # Create your views here.
 from django.shortcuts import render, render_to_response
 from django.http import HttpResponse, HttpResponseBadRequest
-import json, requests, twitter
-
+import json, requests, twitter, urlparse
+from urllib import quote
 
 def createError(msg):
     response_data = { "error" : -1, "message" : msg }
@@ -21,11 +21,42 @@ def twitter_login(request):
     
     baseUrl = "https://api.twitter.com/"
     method = "oauth/request_token"
-    params = {"oauth_callback" : twitter.oauth.urlencode_noplus("twitter")}
+    params = { "oauth_callback" : "http://ec2-54-244-189-248.us-west-2.compute.amazonaws.com/ct/twitter_login_handler/" }
     
     #auth=OAuth(oauth_token, oauth_token_secret, CONSUMER_KEY, CONSUMER_SECRET)
-    auth = OAuth("", "", consumerKey, consumerSecret)
+    stream = twitter.TwitterStream(auth=twitter.UserPassAuth('sbtest5', 'collablab'))
+    iterator = twitter_stream.statuses.filter()
     
-    auth.encode_params(baseUrl, method, params)
+    return createError()
+
+def encodeWord(w):
+    if type(w) is unicode: w = w.encode('utf-8')
+    w = quote(w,safe='').replace("+", "%20")
+    return w
     
-    return createError("it works!")
+def encodeHeaders(query):
+    
+    headers = {}
+    for k,v in query:
+        if type(k) is unicode: k = k.encode('utf-8')
+        if type(v) is unicode: v = v.encode('utf-8')
+        v = quote(v,safe='').replace("+", "%20")
+        headers[k] = v
+        
+    return headers
+
+
+def twitter_login_handler(request):
+    
+    
+    return createError("reached twitter login handler")
+
+
+def parse_oauth_tokens(result):
+    for r in result.split('&'):
+        k, v = r.split('=')
+        if k == 'oauth_token':
+            oauth_token = v
+        elif k == 'oauth_token_secret':
+            oauth_token_secret = v
+    return oauth_token, oauth_token_secret
