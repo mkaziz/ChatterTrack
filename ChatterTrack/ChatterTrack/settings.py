434afa4319ff0f1d908167a1bec501cb99f93ffa
Khalid <kaziz@u.northwestern.edu>
2013-04-23 03:31:21 -0500

    Added db to gitignore and updated so that twitter redirect is now working Signed-off-by: Khalid <kaziz@u.northwestern.edu>

--- ChatterTrack/ChatterTrack/settings.py
+++ ChatterTrack/ChatterTrack/settings.py
@@ -19,7 +19,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'ct-database.db',                      # Or path to database file if using sqlite3.
+        'NAME': os.path.join(PROJECT_PATH, 'ct-database.db').replace('\\','/'),                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',
--- ChatterTrack/ct/views.py
+++ ChatterTrack/ct/views.py
@@ -8,15 +8,24 @@ from django.contrib.auth.decorators import login_required
 
 # python imports
 import json, requests, twitter, urlparse
-from urllib import quote
+from urllib import quote, urlencode
 import oauth2 as oauth
 import cgi
+
+# twitter api urls
+request_token_url = 'http://api.twitter.com/oauth/request_token'
+access_token_url = 'http://api.twitter.com/oauth/access_token'
+authenticate_url = 'http://api.twitter.com/oauth/authenticate'
+
+# OAuth globals 
+consumer = oauth.Consumer(settings.TWITTER_KEY, settings.TWITTER_SECRET)
+client = oauth.Client(consumer)
 
 def createError(msg):
     response_data = { "error" : -1, "message" : msg }
     return HttpResponseBadRequest(content=json.dumps(response_data), content_type="application/json")
   
-@login_required(login_url='/ct/login/') 
+#@login_required(login_url='/ct/login/') 
 def index(request):
     response_data = { "error" : -1, "message" : "test" }
     return render(request, "index.html")
@@ -27,9 +36,18 @@ def login(request):
 
 def twitter_login(request):
     
-    baseUrl = "https://api.twitter.com/"
-    method = "oauth/request_token"
-    params = { "oauth_callback" : "http://ec2-54-244-189-248.us-west-2.compute.amazonaws.com/ct/twitter_login_handler/" }
+    resp, content = client.request(request_token_url, "POST", body="oauth_callback=http://ec2-54-244-189-248.us-west-2.compute.amazonaws.com/ct/index/")
     
-    return createError()
+    if resp['status'] != '200':
+        raise Exception("Invalid response from Twitter.")
+
+    # Step 2. Store the request token in a session for later use.
+    request.session['request_token'] = dict(cgi.parse_qsl(content))
+
+    # Step 3. Redirect the user to the authentication URL.
+    url = "%s?oauth_token=%s" % (authenticate_url,
+        request.session['request_token']['oauth_token'])
+
+    return HttpResponseRedirect(url)
+
 
